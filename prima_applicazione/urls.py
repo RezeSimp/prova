5fa6ce755aded4b28f6853c31d6753e9d00c1823
RezeSimp <federico.falcone3105@gmail.com>
2021-10-29 13:01:08 +0200

    livello 3

--- prima_applicazione/urls.py
+++ prima_applicazione/urls.py
@@ -1,10 +1,11 @@
 from os import name
 from django.urls import path
-from prima_applicazione.views import homepage,welcome,lista
+from prima_applicazione.views import homepage,welcome,lista,chi_siamo
 
 app_name="prima_applicazione"
 urlpatterns=[
     path('', homepage, name='homepage'),
     path('welcome', welcome, name="welcome.html"),
     path('lista', lista, name="lista"),
+    path('chi_siamo', chi_siamo, name="chi_siamo"),
 ]
