a393112074fc07070efcac211110a1dfdbad9d4f
RezeSimp <federico.falcone3105@gmail.com>
2021-11-04 10:55:07 +0100

    Livello 4

--- prima_applicazione/urls.py
+++ prima_applicazione/urls.py
@@ -1,6 +1,6 @@
 from os import name
 from django.urls import path
-from prima_applicazione.views import homepage,welcome,lista,chi_siamo
+from prima_applicazione.views import homepage,welcome,lista,chi_siamo,variabili
 
 app_name="prima_applicazione"
 urlpatterns=[
@@ -8,4 +8,5 @@ urlpatterns=[
     path('welcome', welcome, name="welcome.html"),
     path('lista', lista, name="lista"),
     path('chi_siamo', chi_siamo, name="chi_siamo"),
+    path("variabili", variabili, name="variabili"),
 ]
