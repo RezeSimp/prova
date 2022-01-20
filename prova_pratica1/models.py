880a2a5f0b4e7685fd1e157e9c681eca82a310dd
RezeSimp <federico.falcone3105@gmail.com>
2022-01-20 11:32:29 +0100

    4

--- prova_pratica1/models.py
+++ prova_pratica1/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+class Materia(models.Model):
+    materia=models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.nome + " " + self.materia
+
+class Voti(models.Model):
+    nome=models.CharField(max_length=20)
+    voto=models.CharField(max_length=2)
+    assenza=models.CharField(max_length=3)
+    materia=models.ForeignKey(Materia, on_delete=models.CASCADE, related_name="materie")
+
+    def __str__(self):
+        return self.nome + " " + self.materia + " " + self.voto + " " + self.assenza
